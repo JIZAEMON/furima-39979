b0998a52bcd3ccfc48e2aca45982692bbaccc2eb
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-03 15:45:00 +0900

    新規登録時エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers'
        
   validates :nickname, presence: true
 
   validates :first_name, presence: true
-  validates_format_of :first_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には漢字、ひらがな、カタカナ、英字、数字、ー（長音記号）のみを使用してください'
+  validates_format_of :first_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'
 
   validates :first_name_kana, presence: true
-  validates_format_of :first_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'にはカタカナ、ー（長音記号）のみを使用してください'
+  validates_format_of :first_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters'
 
   validates :last_name, presence: true
-  validates_format_of :last_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には漢字、ひらがな、カタカナ、英字、数字、ー（長音記号）のみを使用してください'
+  validates_format_of :last_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters'
 
   validates :last_name_kana, presence: true
-  validates_format_of :last_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'にはカタカナ、ー（長音記号）のみを使用してください'
+  validates_format_of :last_name_kana, with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters'
 
   validates :birthday, presence: true
 
