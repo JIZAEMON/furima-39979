8e86b5319935e66e32f953c985ed1f5a7a53126a
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-10 19:49:58 +0900

    Itemモデル用factoryボットおよびRspecファイルの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                      {Faker::Commerce.product_name}
+    introduce                 {Faker::Lorem.sentence}
+    category_id               {rand(2..11)}
+    condition_id              {rand(2..7)}
+    shipping_charge_payer_id  {rand(2..3)}
+    prefecture_id             {rand(1..47)}
+    days_to_ship_id           {rand(2..4)}
+    price                     {rand(300..9999999)}
+
   end
 end
