63b4aeffcec29281d0457f40750cc7809447499f
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-10 15:32:35 +0900

    itemコントローラーのアクションに、itemレコードを保存する処理を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduce, :category_id)
+  end
+
 end
