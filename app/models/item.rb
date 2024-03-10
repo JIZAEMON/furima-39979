8334f80cd6bb9d3850f52b4a8fd6d3ab4e05acfc
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-10 15:12:32 +0900

    Itemモデルの作成、カラムの記述、マイグレーションの実行、モデルへバリデーションとアソシエーションの書き込み

---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  validates :name,                      presence: true
+  validates :introduce,                 presence: true
+  validates :category_id,               presence: true
+  validates :condition_id,              presence: true
+  validates :shipping_charge_payer_id,  presence: true
+  validates :prefecture_id,             presence: true
+  validates :days_to_ship_id,           presence: true
+  validates :price,                     presence: true
+
+  belongs_to :user
+  has_one_attached :image
+end
