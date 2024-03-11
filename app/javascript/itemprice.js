20ce9528126d61e517180ba31bd8aa34b6ad0500
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-11 16:15:46 +0900

    修正2 値段入力欄に数値が入力された時それを取得する確認

--- app/javascript/itemprice.js
+++ app/javascript/itemprice.js
@@ -1,8 +1,10 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+ })
+ 
 });
 
-priceInput.addEventListener("input", () => {
-   const inputValue = priceInput.value;
-   console.log(inputValue);
-})
