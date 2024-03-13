08f669c524e6750f5772c2ff7439655616dcd5bf
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-13 19:01:54 +0900

    ShippingAddressモデルのカラムを記述

--- db/migrate/20240313095423_create_shipping_addresses.rb
+++ db/migrate/20240313095423_create_shipping_addresses.rb
@@ -1,6 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :shipping_addresses do |t|
+      t.string      :postal_code,       null: false, default: ""
+      t.integer     :prefecture_id,     null: false
+      t.string      :city,              null: false, default: ""
+      t.string      :street_number,     null: false, default: ""
+      t.string      :building_name,     null: false, default: ""
+      t.string      :phone_number,      null: false, default: ""
+      t.references  :order,             null: false, foreign_key: true
 
       t.timestamps
     end
