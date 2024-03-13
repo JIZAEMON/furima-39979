f6048ca8c8329b5e73cd33a2a1e75b26adc8f705
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-13 19:11:04 +0900

    OrderモデルとShippingAddresモデルのバリデーションを記載

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,21 @@
 class ShippingAddress < ApplicationRecord
+
+  # 空欄を許可しない
+  validates :postal_code,                 presence: true
+  validates :prefecture_id,               presence: true
+  validates :city,                        presence: true
+  validates :street_number,               presence: true
+  validates :building_name,               presence: true
+  validates :phone_number,                presence: true
+
+  # アクティブハッシュを使うカラムは数字のみであり、初期選択肢の"---"では保存できない
+  validates :prefecture_id,               numericality: { other_than: 0 , message: "can't be blank"}
+
+  # 他モデルとのアソシエーションを記述
+  belongs_to :order
+
+  # アクティブハッシュとのアソシエーションを記述
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
 end
