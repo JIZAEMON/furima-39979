298e0c1c1f0c00a4745169387f227ece517b3340
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-13 21:59:52 +0900

    デバッグ用処理を記述3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,9 +10,9 @@ class OrdersController < ApplicationController
     if @order_shipping_address.valid?
       redirect_to root_path#デバッグ用
       @order_shipping_address.save
-      redirect_to new_item_path
+      redirect_to root_path
     else
-      redirect_to "https://www.google.co.jp/"#デバッグ用
+      redirect_to new_item_path#デバッグ用
       render :index, status: :unprocessable_entity
     end
   end
