8f9fc2f3160677fea6de7dc8a1ee2cc7327aefe0
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-13 20:28:32 +0900

    デバッグ用コメントアウト

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -40,15 +40,15 @@ class OrderShippingAddress
     order = Order.create(user_id: user_id, item_id: item_id)
 
     # ShippingAddressレコード生成
-    ShippingAddress.create(
-      postal_code: postal_code, 
-      prefecture: prefecture, 
-      city: city, 
-      street_number: street_number, 
-      building_name: building_name, 
-      phone_number: phone_number,
-      order_id: order.id
-    )
+    # ShippingAddress.create(
+    #   postal_code: postal_code, 
+    #   prefecture: prefecture, 
+    #   city: city, 
+    #   street_number: street_number, 
+    #   building_name: building_name, 
+    #   phone_number: phone_number,
+    #   order_id: order.id
+    # )
   end
 
 end
