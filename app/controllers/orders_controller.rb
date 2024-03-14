4edaae0857263237ecfb213b61adfa48dd68c80b
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-14 15:13:07 +0900

    ordersコントローラーに処理を追加:不適切な場合遷移
    1.非ログインユーザーをログインページへ遷移 2.商品が売却済みでorderを保有している場合にトップページへ遷移 3.商品の出品者と閲覧ユーザーが同じ場合にトップページへ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,12 @@
 class OrdersController < ApplicationController
 
+  # 非ログインユーザーをログインページへ遷移
+  before_action :authenticate_user!
+
+  # 商品が売却済みでorderを保有している場合、または商品の出品者と閲覧ユーザーが同じ場合にトップページへ遷移
+  bedore_action :redirect_to_root_if_item_sold
+  bedore_action :redirect_to_home_if_seller_viewing
+
   def index
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
@@ -18,6 +25,17 @@ class OrdersController < ApplicationController
 
   private
 
+  # 商品が売却済みでorderを保有している場合にトップページへ遷移
+  def redirect_to_root_if_item_sold
+    redirect_to root_path if @item.order
+  end
+
+  # 商品の出品者と閲覧ユーザーが同じ場合にトップページへ遷移
+  def redirect_to_home_if_seller_viewing
+    redirect_to root_path if @item.user == current_user
+  end
+
+  # formオブジェクトOrderShippingAddress用にパラメータを許可・merge
   def order_shipping_address_params
     params.require(:order_shipping_address).permit(
       :postal_code, 
