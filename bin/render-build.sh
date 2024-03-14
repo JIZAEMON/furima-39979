8938408a92ac8788c6bb52d0a4f46645f5f5f84d
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-14 14:30:01 +0900

    RenderデプロイごとのDBリセットを無効化

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,7 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate # DBリセットのため一時的にコメントアウト
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset # DBリセット
+
+
+bundle exec rake db:migrate # Render上DBリセット時は、この行をコメントアウトし下の行の"DISABLE..."を有効化
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
