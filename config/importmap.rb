9a481b5220194b71dfad9b6fa6c21580b0f9555e
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-14 18:55:24 +0900

    javascriptの導入、コンソールにメッセージ表示

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "itemprice", to: "itemprice.js"
+pin "card", to: "card.js"
