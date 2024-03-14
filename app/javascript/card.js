e54f65c40d0735f9c974ab1fbd6ac0246f7ea256
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-14 19:21:33 +0900

    購入ボタンクリック時に発火を確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,11 @@
 const pay = () => {
-  const publicKey = gon.public_key
-  console.log("カード情報トークン化のためのJavaScript");
+
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) => {
+    console.log("フォーム送信時にイベント発火")
+    e.preventDefault();
+  });
+  
 };
 
 window.addEventListener("turbo:load", pay);
