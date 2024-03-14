6cd8b2de2283024bc31390901cfad391f191a699
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-14 17:35:11 +0900

    1.OrderShippingAddressモデルの単体テストコード記述 2.pry-railsの導入

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :order_shipping_address do
-    postal_code       {"123-1234"}
-    prefecture_id     {5}
-    city              {"Dcity"}
-    street_number     {"Dstreet_number"}
-    building_name     {"Dbuilding_name"}
-    phone_number      {"00011112222"}
-    order_id          {5}
-    item_id           {5}
-    user_id           {5}
+
+    postal_code       {"#{sprintf("%03d", rand(0..999))}-#{sprintf("%04d", rand(0..9999))}"}
+    prefecture_id     {rand(1..47)}
+    city              {Faker::Address.city}
+    street_number     {Faker::Address.street_address}
+    building_name     {Faker::Address.secondary_address}
+    phone_number      {"#{sprintf("%011d", rand(0..99999999999))}"}
+    
   end
 end
