f95a8b40aad472090f3d14acd53b1133744413f6
JIZAEMON <msk5482@yahoo.co.jp>
2024-03-15 18:35:05 +0900

    データベースのリセット処理記述

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
-bundle exec rake db:migrate # Render上DBリセット時は、この行をコメントアウトし下の行の"DISABLE..."を有効化
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+# bundle exec rake db:migrate # Render上DBリセット時は、この行をコメントアウトし下の行の"DISABLE..."を有効化
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
